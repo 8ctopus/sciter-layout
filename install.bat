0a1147578d7c4e2d90e63f64e724c0053396466c
8ctopus <hello@octopuslabs.io>
2021-11-01 15:29:04 +0500

    updated: directory architecture for mac version

--- install.bat
+++ install.bat
@@ -1,3 +1,7 @@
+mkdir bin\win-x32
+
+cd bin\win-x32
+
 REM sciter 4.4.8.15
 curl -LO https://github.com/c-smile/sciter-js-sdk/raw/faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin/windows/x32/scapp.exe
 curl -LO https://github.com/c-smile/sciter-js-sdk/raw/faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin/windows/x32/inspector.exe
